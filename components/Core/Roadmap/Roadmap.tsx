6248c039c9ac71f670314452148fdea4c7a07f9a
Theo <theo.pessegue2@gmail.com>
2023-01-08 22:56:54 +0100

    element without transition and text

--- components/Core/Roadmap/Roadmap.tsx
+++ components/Core/Roadmap/Roadmap.tsx
@@ -94,53 +94,6 @@ const CarreDeNavigation = (props: RoadmapNavigation) => {
   );
 };
 
-function Carousel(props: any) {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const images = props.images;
-
-  function goToPrevSlide() {
-    setCurrentIndex(currentIndex - 1);
-  }
-
-  function goToNextSlide() {
-    setCurrentIndex(currentIndex + 1);
-  }
-
-  return (
-    <div className="relative h-64">
-      <button
-        className="absolute left-0 top-0 p-2 rounded-l-md text-gray-600 hover:text-gray-800 focus:outline-none focus:shadow-outline-blue"
-        onClick={goToPrevSlide}
-        disabled={currentIndex === 0}>
-        <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-          <path
-            d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-            clipRule="evenodd"
-            fillRule="evenodd"
-          />
-        </svg>
-      </button>
-      <img
-        className="w-full h-full object-cover"
-        src={images[currentIndex]}
-        alt="Image du carrousel"
-      />
-      <button
-        className="absolute right-0 top-0 p-2 rounded-r-md text-gray-600 hover:text-gray-800 focus:outline-none focus:shadow-outline-blue"
-        onClick={goToNextSlide}
-        disabled={currentIndex === images.length - 1}>
-        <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-          <path
-            d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-            clipRule="evenodd"
-            fillRule="evenodd"
-          />
-        </svg>
-      </button>
-    </div>
-  );
-}
-
 export const Roadmap = () => {
   const [indexRoadmap, setCompteur] = useState(0);
   const addIndex = () => {
@@ -161,11 +114,8 @@ export const Roadmap = () => {
         <h1 className="text-center text-white uppercase text-[40px] pt-2 pb-2 sm:text-[40px] sm:pt-2">
           Roadmap
         </h1>
-        <li>
-          `${returnRightTranslate()}, translate-x-[-
-          {indexRoadmap}00%]
-        </li>
-        <div className="h-[325px] w-[300px] md:w-[600px] rounded-lg mx-auto relative overflow-hidden">
+
+        <div className="h-[325px] w-[300px] md:w-[600px] rounded-lg mx-auto relative ">
           <div className={`${returnRightTranslate()} transform transition-all duration-700 linear`}>
             <div
               className={`${listOfRoadmapElm[0].backgroundImageDef} h-[325px] w-[300px] 
@@ -192,9 +142,13 @@ export const Roadmap = () => {
               transition-all duration-500 linear
               transform translate-x-[400%]`}></div>
           </div>
-
+          <div className="relative bg-[#ececec] h-1 rounded-full">
+            <div
+              className={`h-full w-full bg-gradient-to-r from-[#b8a7fc] via-[#48b1c5] to-[#008eab] 
+              transition-all duration-500 ease-out inset-0 absolute rounded-full translate-x-[-100%]`}></div>
+          </div>
           <div className="">
-            <div className="flex space-x-[235px] mt-[20px] mb-4 place-content-center">
+            <div className="flex space-x-[235px] mb-4 mt-[20px] place-content-center">
               <div className="scale-x-[-1]">
                 <CarreDeNavigation
                   indexNavigation={indexRoadmap}
@@ -215,7 +169,6 @@ export const Roadmap = () => {
           </div>
         </div>
       </div>
-      <Carousel images={['image1.jpg', 'image2.jpg', 'image3.jpg']} />
     </>
   );
 };
