d40af33959bd91fe91ec04903e0e29a6ea3acf45
Shannorr <theo.pessegue2@gmail.com>
2023-01-09 11:56:53 +0100

    up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,56 +1,52 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       transitionProperty: {
-        'navBar' : 'margin-left, margin-right, border-radius, height',
-        'blueBorderLeft' : 'left, right',
-        'productCard' : 'width, height, transform',
+        navBar: 'margin-left, margin-right, border-radius, height',
+        blueBorderLeft: 'left, right',
+        productCard: 'width, height, transform'
       },
       keyframes: {
-        slidein : {
+        slidein: {
           '0%': { height: '0.2rem', marginRight: '50rem', marginLeft: '50rem', display: 'none' },
           '50%': { height: '0.2rem' },
           '75%': { height: '0.2rem' },
-          '85%': { height: '0.2rem' },
+          '85%': { height: '0.2rem' }
         },
         hideImage: {
           '0%': { opacity: '0' },
           '50%': { opacity: '0' },
-          '95%': { opacity: '1' },
+          '95%': { opacity: '1' }
         },
         hideBlueBorder: {
           '0%': { opacity: '0' },
           '50%': { opacity: '0' },
-          '95%': { opacity: '1' },
+          '95%': { opacity: '1' }
         },
         hideNavBarContent: {
           '0%': { opacity: '0' },
           '50%': { opacity: '0' },
-          '95%': { opacity: '1' },
+          '95%': { opacity: '1' }
         },
         textTransition: {
           '0%': { transform: 'translateY(50%)', opacity: '0' },
-          '100%': { transform: 'translateY(0%)' },
+          '100%': { transform: 'translateY(0%)' }
         },
         textTransition2: {
           '0%': { transform: 'translateY(30%)', opacity: '0' },
           '50%': { transform: 'translateY(30%)', opacity: '0' },
-          '100%': { transform: 'translateY(0%)' },
+          '100%': { transform: 'translateY(0%)' }
         },
         imageTransition: {
           '0%': { transform: 'scale(0.5)' },
-          '100%': { transform: 'scale(1)' },
+          '100%': { transform: 'scale(1)' }
         },
         starBackground: {
           '0%': { transform: 'scale(1)' },
-          '100%': { transform: 'scale(1.5)' },
-        },
-        
+          '100%': { transform: 'scale(1.5)' }
+        }
       },
       animation: {
         slidein: 'slidein 1.2s',
@@ -60,9 +56,14 @@ module.exports = {
         textTransition: 'textTransition 1.2s',
         textTransition2: 'textTransition2 0.8s',
         imageTransition: 'imageTransition 2.6s',
-        starBackground: 'starBackground 10s alternate infinite',
+        starBackground: 'starBackground 10s alternate infinite'
       }
     },
+    backgroundImage: {
+      'roadmap-tunnel': 'url(/static/images/Roadmap/roadmap_tunnel_futuriste.svg)',
+      'roadmap-man-silouette': 'url(/static/images/Roadmap/roadmap_man_silouette.svg)',
+      'roadmap-hexagone-nft': 'url(/static/images/Roadmap/roadmap_hexagone_nft.svg)'
+    }
   },
-  plugins: [],
+  plugins: []
 };
